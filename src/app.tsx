627f0acb2359d731d3c19cc9fb11e90934b1bce3
nboraxxd <nhatbaoxxd@gmail.com>
2024-09-05 23:07:01 +0700

    chore: Add Netflix Sans font

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 import '@/globals.css'
-import { cn } from '@/lib/utils'
+import { cn } from '@/utils'
 
 export default function App() {
-  return <p className={cn('text-red-500')}>Hello world</p>
+  return <p className={cn('text-red-500 font-bold text-5xl')}>Hello world</p>
 }
