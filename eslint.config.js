393ab3e603d91c7f27ccb2759283f0794996a35e
nboraxxd <nhatbaoxxd@gmail.com>
2024-09-05 22:35:59 +0700

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,58 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import tailwind from 'eslint-plugin-tailwindcss'
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      eslintPluginPrettierRecommended,
+      ...tailwind.configs['flat/recommended'],
+    ],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          varsIgnorePattern: '^_',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      'tailwindcss/no-custom-classname': ['warn', { whitelist: ['mySwiper'] }],
+      'prettier/prettier': [
+        'warn',
+        {
+          arrowParens: 'always',
+          semi: false,
+          trailingComma: 'es5',
+          tabWidth: 2,
+          endOfLine: 'auto',
+          useTabs: false,
+          singleQuote: true,
+          printWidth: 120,
+          jsxSingleQuote: false,
+        },
+      ],
+    },
+    settings: {
+      tailwindcss: {
+        classRegex: '.*[Cc]lass[Nn]ame.*',
+      },
+    },
+  }
+)
---
+++ src/app.tsx
@@ -0,0 +1,6 @@
+import '@/globals.css'
+import { cn } from '@/lib/utils'
+
+export default function App() {
+  return <p className={cn('text-red-500')}>Hello world</p>
+}
