277d9f176a2f14adf0ab0834cea96264141863bb
Bora <nhatbaoxxd@gmail.com>
2025-02-03 16:04:32 +0700

    refactor: Remove unused API call and clean up imports in TrendingSection component

--- src/components/trending-section.tsx
+++ src/components/trending-section.tsx
@@ -1,22 +1,14 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 import { TRENDING_TABS } from '@/constants'
 import { TrendingParamsType } from '@/lib/schemas/trending.shema'
 import { useGetTrendingQuery } from '@/lib/tanstack-query/use-medias'
 import MediaTabs from '@/components/media-tabs'
-import http from '@/utils/http'
 
 export default function TrendingSection() {
   const [trendingType, setTrendingType] = useState<TrendingParamsType['trendingType']>(TRENDING_TABS[0].value)
 
   const { data, isLoading, isSuccess } = useGetTrendingQuery({ trendingType, timeWindow: 'day' })
-
-  useEffect(() => {
-    ;(async () => {
-      const provices = await http.get('/p', { baseURL: 'https://provinces.open-api.vn/api' })
-      console.log('🔥 ~ ; ~ provices:', provices)
-    })()
-  }, [])
 
   return (
     <MediaTabs
